025cc08ff75e341055d651b0a12d8d6322ba9cc3
Hoang Minh Thang <p@banphim.net>
2013-07-15 01:58:43 +0700

    bump to v1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject chlorine-utils "1.1.0"
+(defproject chlorine-utils "1.2.0"
   :description "Utility library for Chlorine projects"
   :url "http://github.com/chlorinejs/chlorine-utils"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [pathetic "0.4.0" :exclusions [org.clojure/clojure]]
                  ])
